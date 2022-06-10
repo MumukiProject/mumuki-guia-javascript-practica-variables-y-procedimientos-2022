43c73a896fedc7f629cf7269ea819729aa11427d
mayra@mumuki.org <mayra@mumuki.org>
2022-06-10 16:44:25 +0000

    Mumuki Export on 2022-06-10 16:44:25 +0000

--- 00003_Destino alternativo/test..js
+++ 00003_Destino alternativo/test..js
@@ -1,5 +1,4 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
+  it("Si tenemos un pozo acumulado de 7500 y viajan dos personas nos retorna como destino Tilcara", function() {
+    pozo = 7500
+    assert.equal(true)
   })
-})
