24b89dc23edfe1acd2faadc952482ef4f8341f97
mayra@mumuki.org <mayra@mumuki.org>
2022-06-14 15:11:04 +0000

    Mumuki Export on 2022-06-14 15:11:04 +0000

--- 00009_Representando/test..js
+++ 00009_Representando/test..js
@@ -2,3 +2,8 @@
     declararRepresentante('Perla', 5, 'Enzo', 2)
     assert.equal(representante, 'Perla')
   })
+
+  it("Si tenemos a Theo con 8 votos y a Pipi con 4, la variable representante toma como valor 'Theo' ", function() {
+    declararRepresentante('Theo', 8, 'Pipi', 4)
+    assert.equal(representante, 'Theo')
+  })
