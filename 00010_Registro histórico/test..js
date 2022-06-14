353267b1ce19cbc08fecf4acf091989d461f0f20
mayra@mumuki.org <mayra@mumuki.org>
2022-06-14 18:04:52 +0000

    Mumuki Export on 2022-06-14 18:04:52 +0000

--- 00010_Registro histórico/test..js
+++ 00010_Registro histórico/test..js
@@ -1,3 +1,4 @@
-  it("Descripción del ejemplo", function() {
-    assert(true)
+  it("Si registramos como representante del año 2021 a Sol se agrega a la variable global representantesPorAnio", function() {
+    registrarRepresentanteDelAnio("Sol", 2021)
+    assert.deepEqual(representantesPorAnio, ["Sol" 2021])
   })
