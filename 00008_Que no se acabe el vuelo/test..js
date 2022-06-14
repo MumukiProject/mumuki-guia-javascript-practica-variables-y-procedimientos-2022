b1bac336b96399f07476ba0664bbccf89d444bac
mayra@mumuki.org <mayra@mumuki.org>
2022-06-14 13:59:30 +0000

    Mumuki Export on 2022-06-14 13:59:30 +0000

--- 00008_Que no se acabe el vuelo/test..js
+++ 00008_Que no se acabe el vuelo/test..js
@@ -1,3 +1,23 @@
-  it("Descripción del ejemplo", function() {
-    assert(true)
+  it("Si tenemos un pozo de 900 pesos y hacemos volverAEmpezar el pozo queda en 0 pesos", function() {
+    pozo = 900
+    volverAEmpezar()
+    assert.equal(pozo, 0)
+  })
+  
+  it("Si tenemos un pozo de 500 pesos y hacemos volverAEmpezar el pozo queda en 0 pesos", function() {
+    pozo = 500
+    volverAEmpezar()
+    assert.equal(pozo, 0)
+  })
+  
+  it("Si tenemos un pozo de 1500 pesos y hacemos volverAEmpezar el pozo no se reinicia", function() {
+    pozo = 1500
+    volverAEmpezar()
+    assert.equal(pozo, 1500)
+  })
+  
+  it("Si tenemos un pozo de 9000 pesos y hacemos volverAEmpezar el pozo no se reinicia", function() {
+    pozo = 9000
+    volverAEmpezar()
+    assert.equal(pozo, 9000)
   })
