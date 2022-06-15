0b57cf22d5d4ff6c7f1f66390c1a1bfbf791a15c
mayra@mumuki.org <mayra@mumuki.org>
2022-06-15 18:12:21 +0000

    Mumuki Export on 2022-06-15 18:12:21 +0000

--- 00007_La tercera es la vencida/test..js
+++ 00007_La tercera es la vencida/test..js
@@ -21,3 +21,21 @@
     assert.equal(pozo, 98500)
   })
   
+  describe("Si tenemos 100000 en el pozo y aplicamos el IVG el pozo queda en 99500", function() {
+  it("Descripción del ejemplo", function() {
+    pozo = 100000
+    aplicarIvg()
+    assert.equal(pozo, 99500)
+  })
+})
+
+  describe("Si tenemos 100000 en el pozo y aplicamos el IVG tres veces el pozo queda en 98500", function() {
+  it("Descripción del ejemplo", function() {
+    pozo = 100000
+    aplicarIvg()
+    aplicarIvg()
+    aplicarIvg()
+    assert.equal(pozo, 98500)
+  })
+})
+  
