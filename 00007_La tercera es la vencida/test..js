c81aff934613e8544f19c3d773e375ed2a14c9ad
gustavo@mumuki.org <gustavo@mumuki.org>
2022-06-15 17:42:59 +0000

    Mumuki Export on 2022-06-15 17:42:59 +0000

--- 00007_La tercera es la vencida/test..js
+++ 00007_La tercera es la vencida/test..js
@@ -1,7 +1,22 @@
- 
+  it("Si tenemos 100000 en el pozo y aplicamos el IVG el pozo queda en 99500", function() {
+    pozo = 100000
+    contador = 0
+    aplicarIvg()
+    assert.equal(pozo, 99500)
+  })
+  
+  it("Si tenemos 100000 en el pozo y aplicamos el IVG tres veces el pozo queda en 98500", function() {
+    pozo = 100000
+    contador = 0
+    aplicarIvg()
+    aplicarIvg()
+    aplicarIvg()
+    assert.equal(pozo, 98500)
+  })
   
   it("Si tenemos 100000 en el pozo y aplicamos el IVG cuatro veces el pozo queda en 98500", function() {
     pozo = 100000
+    contador = 0
     aplicarIvg()
     aplicarIvg()
     aplicarIvg()
