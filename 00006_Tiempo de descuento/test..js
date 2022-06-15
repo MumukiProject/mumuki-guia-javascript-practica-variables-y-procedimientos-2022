fa36e8b70a2568ef5eea32ebac71d3d18f701fce
mayra@mumuki.org <mayra@mumuki.org>
2022-06-15 14:16:25 +0000

    Mumuki Export on 2022-06-15 14:16:25 +0000

--- 00006_Tiempo de descuento/test..js
+++ 00006_Tiempo de descuento/test..js
@@ -1,16 +1,16 @@
   it("Si tenemos un pozo de 1000 pesos el monto del IVG es 0 porque no excede los 15000 pesos", function() {
     pozo = 1000;
-    assert.equal(calcularMontoIvg, 0)
+    assert.equal(calcularMontoIvg(), 0)
   })
   
   it("Si tenemos un pozo de 16000 pesos el monto del IVG es 160", function() {
     pozo = 16000;
-    assert.equal(calcularMontoIvg, 160)
+    assert.equal(calcularMontoIvg(), 160)
   })
   
   it("Si tenemos un pozo de 125000 pesos el monto del IVG es 500", function() {
     pozo = 125000;
-    assert.equal(calcularMontoIvg, 500)
+    assert.equal(calcularMontoIvg(), 500)
   })
   
   it("Si tenemos un pozo de 16000 pesos y aplicamos el IVG el monto del pozo es 15840", function() {
